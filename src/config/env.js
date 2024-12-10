58ae900d792b73272c739bd54227fced986f77f6
Mattia Warning <m.warning@robeco.nl>
2024-12-10 14:37:04 +0100

    second commit contact deploy

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,8 @@
 // src/config/env.js
-export const API_KEY = import.meta.env.VITE_API_KEY;
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const API_KEY = process.env.VUE_APP_API_KEY;
+export const SUPABASE_URL = process.env.VUE_APP_SUPABASE_URL;
+export const SUPABASE_ANON_KEY = process.env.VUE_APP_SUPABASE_ANON_KEY;
 
-export const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-export const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-export const EMAILJS_USER_ID = import.meta.env.VITE_EMAILJS_USER_ID;
+export const EMAILJS_SERVICE_ID = process.env.VUE_APP_EMAILJS_SERVICE_ID;
+export const EMAILJS_TEMPLATE_ID = process.env.VUE_APP_EMAILJS_TEMPLATE_ID;
+export const EMAILJS_USER_ID = process.env.VUE_APP_EMAILJS_USER_ID;
